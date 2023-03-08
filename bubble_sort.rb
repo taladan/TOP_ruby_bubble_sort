1b9244b182afd438bb54b53576fa62741f7db1b6
D.J. Crosby <taladan@gmail.com>
2023-03-08 13:04:56 -0500

    Add loop and conditional to iterate needed times
    Iterate through array until we stop swapping elements

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,25 +1,28 @@
 # bubble_sort.rb
 
 def bubble_sort(arr)
-  # Compare pairs of elements of an array.  If the right element is larger than the left element, swap them
-  # do this for every element of the array and continue to iterate over the array until it is completely sorted
-
-  #each_cons will move consecutively through an array
-  arr
-    .each_cons(2)
-    .to_a
-    .each_with_index do |comparison, index|
-      if comparison[1] < comparison[0]
-        # comparison[0], comparison[1] = comparison[1], comparison[0]
-        arr[index], arr[index + 1] = arr[index + 1], arr[index]
-        # p comparison
-        # p index
+  loop do
+    # Need to set up correct did_swap conditions
+    did_swap = false
+    arr
+      .each_cons(2)
+      .to_a
+      .each_with_index do |comparison, index|
+        if comparison[1] < comparison[0]
+          did_swap = true
+          arr[index], arr[index + 1] = arr[index + 1], arr[index]
+        end
       end
-    end
+    break if did_swap == false
+  end
 end
 
 test_array = [4, 3, 78, 2, 0, 2]
-
+test_array2 = Array.new(50) { rand(1..100) }
 p test_array
 bubble_sort(test_array)
 p test_array
+
+p test_array2
+bubble_sort(test_array2)
+p test_array2
